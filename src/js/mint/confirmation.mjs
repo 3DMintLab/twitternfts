f302890a81b3189a76ec4ad80af414693ce389e8
cheffNFT <cheffwallets@gmail.com>
2021-08-15 19:27:17 +0100

    improves confirmation.

--- src/js/mint/confirmation.mjs
+++ src/js/mint/confirmation.mjs
@@ -7,6 +7,5 @@ window.$ = window.jQuery = import("jquery");
     console.log(`ipfs: ${ipfs} transaction ${transaction} https://ipfs.io/ipfs/${ipfs}`);
     document.getElementById('picture').setAttribute('src', `https://ipfs.io/ipfs/${ipfs}`);
     document.getElementById("ipfs").innerHTML = `IPFS Code: ${ipfs}`;
-    // document.getElementById("hash").innerHTML = `<a href="https://explorer.cardano-testnet.iohkdev.io/en/transaction?id=${transaction}" target="_blank">Transaction ${transaction}</a>`;
     document.getElementById("hash").innerHTML = `<a href="https://explorer.cardano.org/en/transaction?id=${transaction}" target="_blank">Transaction ${transaction}</a>`;
 ***REMOVED***
